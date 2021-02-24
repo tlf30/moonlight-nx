4936f3c43a5259129632abf85b14e376792f2d08
Trevor Flynn <trevorflynn@liquidcrystalstudios.com>
2021-02-23 16:53:35 -0800

    Fix SSL issue as described in issue #52

--- src/ui/windows/MainWindow.cpp
+++ src/ui/windows/MainWindow.cpp
@@ -74,6 +74,23 @@ void MainWindow::reload() {
                     reload();
                 });
                 alert->add_button("Cancel");
+				
+				alert->add_button("Pair", [this, button] {
+                   auto loader = add<LoadingOverlay>("Pairing... (Enter 0000)");
+                    
+                    GameStreamClient::instance().pair(button->host().address, "0000", [this, loader](auto result){
+                        loader->dispose();
+                        
+                        if (result.isSuccess()) {
+                            reload();
+                        } else {
+                            screen()->add<Alert>("Error", result.error());
+                        }
+                    });
+                });
+				alert->add_button("Force Connect (Use if already paired)", [this, button] {
+                    push<AppListWindow>(button->host().address);
+                });
             }
         });
     }
